86a5c5f9b244845490025879a68dc8cde704e392
Keylenn <darklight_on@163.com>
2022-04-17 21:57:58 +0800

    feat: v2

--- src/components/StepOption.tsx
+++ src/components/StepOption.tsx
@@ -4,12 +4,13 @@ export default function StepOption({
   nextText = "Next",
   backText = "Back",
   disabledBack = false,
+  disabledNext = false,
   onNext,
   onBack
 }: any) {
   return (
     <div>
-      <Button variant="contained" onClick={onNext} sx={{ mt: 1, mr: 1 }}>
+      <Button disabled={disabledNext} variant="contained" onClick={onNext} sx={{ mt: 1, mr: 1 }}>
         {nextText}
       </Button>
       <Button disabled={disabledBack} onClick={onBack} sx={{ mt: 1, mr: 1 }}>
